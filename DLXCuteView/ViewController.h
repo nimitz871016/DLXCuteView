b2374990fa56c24c990899e9d5a8d6216431b8b8
nimitz871016 <test@RuijieMac-mini.local>
2015-04-02 15:40:03 +0800

    initial commit

--- DLXCuteView/ViewController.h
+++ DLXCuteView/ViewController.h
@@ -7,8 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "KYCuteView.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <KYCuteViewDelegate>
 
 
 @end
--- DLXCuteView/ViewController.m
+++ DLXCuteView/ViewController.m
@@ -2,11 +2,13 @@
 //  ViewController.m
 //  DLXCuteView
 //
-//  Created by test on 15-4-2.
+//  Created by duanlingxiu on 15-4-2.
 //  Copyright (c) 2015年 Fujian Ruijie Networks Co., Ltd. All rights reserved.
 //
 
+
 #import "ViewController.h"
+#import "KYCuteView.h"
 
 @interface ViewController ()
 
@@ -14,14 +16,37 @@
 
 @implementation ViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    KYCuteView *cuteView = [[KYCuteView alloc]initWithPoint:CGPointMake(25, 300) superView:self.view];
+    cuteView.viscosity  = 12;
+    cuteView.bubbleWidth = 35;
+    cuteView.bubbleColor = [UIColor colorWithRed:0 green:0.722 blue:1 alpha:1];
+    [cuteView setUp];
+    [cuteView addGesture];
+    [cuteView setDelegate:self];
+    //注意：设置 'bubbleLabel.text' 一定要放在 '-setUp' 方法之后
+    //Tips:When you set the 'bubbleLabel.text',you must set it after '-setUp'
+    cuteView.bubbleLabel.text = @"13";
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (void) viewDidAppear:(BOOL)animated{
+    
+    [super viewDidAppear:animated];
+    NSLog(@"1");
+}
+
+-(void)onUnPlug{
+    NSLog(@"拔掉了");
 }
 
 @end
